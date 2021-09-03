6f343c0f89714b89fa8e95e6d44d8cd7693d5962
Ashley Burns <Ash_burn999@yahoo.com>
2021-09-03 12:04:32 -0400

    installed dependancys, wrote start and server script.. hooked up index, server, and .env.. also got GET projects working properly

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,25 @@
-// Write your "projects" router here!
+const express = require('express')
+const Project = require('./projects-model')
+const router = express.Router()
+
+router.get('/projects', (req, res, next) => {
+    Project.get()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(next)
+})
+
+
+
+//***********************500 error middleware***********//
+//eslint-disable-next-line
+router.use((err, req, res, next) => {
+    console.log(err.message); // delete after
+    res.status(err.status || 500).json({
+        message: err.message,
+        devMessage: 'Something bad inside the projects router!'
+    });
+});
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const cors = require('cors')
+const morgan = require('morgan')
+//const actionsRouter = require('./actions/actions-router')
+const projectsRouter = require('./projects/projects-router')
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+const logger = (req, res, next) => {
+    const formatUrl = `${req.baseUrl}${req.url}`
+    const log = `---------------------------
+    ${Date()} 
+    method: ${req.method}
+    URL: ${formatUrl}
+    status: ${res.statusCode}
+    requester IP: ${req.ip} \n************`
+    console.log(log)
+    next()
+}
+
+server.use(express.json())
+server.use(cors())
+server.use(logger)
+server.use(morgan('dev'))
+//server.use('/api', actionsRouter)
+server.use('/api', projectsRouter)
+
+server.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to Sprint 4!'
+    })
+})
 
 module.exports = server;
